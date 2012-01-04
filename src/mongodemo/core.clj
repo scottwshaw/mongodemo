faeea0ca234448113d8f760276112e07022063f3
Scott Shaw <scottwshaw@gmail.com>
2012-01-04 17:10:43 +1100

    added some functions

--- src/mongodemo/core.clj
+++ src/mongodemo/core.clj
@@ -4,3 +4,23 @@
 (mongo/set-connection! (mongo/make-connection "mydb"))
 
 (mongo/fetch :orders)
+
+;;; tshirts, shorts, thongs
+;;; tshirts: size, color
+;;; shorts: size, color
+;;; thongs: size, color
+
+(defn product [type size color]
+  {:product type :size size :color color})
+
+(defn tshirt [size color]
+  (product :tshirt size color))
+
+(defn random-product []
+  (let [type (rand-nth [:tshirt :shorts :thongs])
+        size (rand-nth [:small :medium :large])
+        color (rand-nth [:red :blue :white :orange :green :black])]
+    (product type size color)))
+
+(random-product)
+
