90e54a53b8ab041b7703c7cbb9aae9f3a06dab98
Scott Shaw <scottwshaw@gmail.com>
2012-01-05 12:29:46 +1100

    Added more stuff around date time

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject mongodemo "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]
+                 [clj-time "0.3.4"]
                  [congomongo "0.1.7"]])
--- src/mongodemo/core.clj
+++ src/mongodemo/core.clj
@@ -1,14 +1,11 @@
 (ns mongodemo.core
-  (:require [somnium.congomongo :as mongo]))
+  (:require [somnium.congomongo :as mongo]
+            [clj-time.core :as time]
+            [clj-time.format :as tformat]))
 
 (mongo/set-connection! (mongo/make-connection "mydb"))
 
 (mongo/fetch :orders)
-
-;;; tshirts, shorts, thongs
-;;; tshirts: size, color
-;;; shorts: size, color
-;;; thongs: size, color
 
 (defn product [type size color]
   {:product type :size size :color color})
@@ -16,11 +13,40 @@
 (defn tshirt [size color]
   (product :tshirt size color))
 
+(def colors [:red :blue :white :orange :green :black])
+(def sizes [:small :medium :large])
+
 (defn random-product []
   (let [type (rand-nth [:tshirt :shorts :thongs])
-        size (rand-nth [:small :medium :large])
-        color (rand-nth [:red :blue :white :orange :green :black])]
+        size (rand-nth sizes)
+        color (rand-nth colors)]
     (product type size color)))
 
-(random-product)
+
+(defn color-mix-50-50 [color]
+  (rand-nth [color (rand-nth colors)]))
+
+(defn random-coordinated-outfit []
+  (let [size (rand-nth sizes)
+        main-color (rand-nth [:red :blue :white :orange :green :black])]
+    [(product :tshirt size (color-mix-50-50 main-color))
+     (product :shorts size (color-mix-50-50 main-color))
+     (product :thongs size (color-mix-50-50 main-color))]))
+
+(defn order-one-outfit [date]
+  {:time date :items (random-coordinated-outfit)})
+
+(defn format-time-for-mongo [date-time-obj]
+  (tformat/unparse (tformat/formatters :date-time) date-time-obj))
+
+(random-coordinated-outfit) 
+(time/date-time 2010 01 01 04 25)
+
+(mongo/insert! :orders (order-one-outfit (time/date-time 2010 01 01 04 25)))
+
+(tformat/show-formatters)
+
+(format-time-for-mongo (time/date-time 2010 01 01 04 25))
+
+
 
